aacdacbead4ede480b7d7c0a956c0640d0573665
ditosyahputra <dito.syahputra@ui.ac.id>
2022-11-17 16:08:07 +0700

    Lab 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutterbaru/form.dart';
+import 'package:flutterbaru/page/form.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterbaru/page/to_do_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -77,6 +78,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 );
               },
             ),
+            ListTile(
+              title: const Text('To Do'),
+              onTap: () {
+                // Route menu ke halaman utama
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const TodoPage()),
+                );
+              },
+            )
           ],
         ),
       ),
